8737d973b2c162a1061fc1e6602387fefb59da1e
itsahmed94 <ahmed94.shabbir@gmail.com>
2021-10-20 18:06:37 +0500

    mm

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ io.on('connection', (socket) => {
 
 const port = process.env.Port || 3000;
 server.listen(port, () => {
+  // MongooseConnect();
   console.log(`Example app listening at http://localhost:${port}`);
-  MongooseConnect();
 });
